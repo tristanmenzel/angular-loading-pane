6440aa11ba85dde32834c80e5e9886fd4221a272
Tristan Menzel <tristan.menzel@readify.net>
2015-05-28 17:33:16 +1000

    Added support for tracking multiple promises.

--- src/loading-pane.js
+++ src/loading-pane.js
@@ -1,29 +1,36 @@
-﻿angular.module('loadingPane',[])
-    .directive('loadingPane', [function() {
+﻿angular.module('loadingPane', [])
+    .directive('loadingPane', [function () {
         return {
             restrict: 'E',
             replace: true,
             transclude: true,
-            scope: { tracker: '=' },
+            scope: {tracker: '='},
             templateUrl: 'loading-pane.tpl.html'
         };
     }])
-    .factory('loading', [function() {
+    .factory('loading', [function () {
         function WorkTracker(isComplete) {
             var self = this;
             self.complete = isComplete !== false;
-            self.track = function(promise) {
+            self.activePromises = 0;
+            self.track = function (promise) {
                 self.complete = false;
-                return promise.then(function() {
-                    self.complete = true;
-                }, function() {
-                    self.complete = true;
+                self.activePromises++;
+                return promise.then(function () {
+                    self.completePromise();
+                }, function () {
+                    self.completePromise();
                 });
+            };
+
+            self.completePromise = function () {
+                self.activePromises = Math.max(0, self.activePromises - 1);
+                self.complete = self.activePromises === 0;
             };
         }
 
         return {
-            createTracker: function(isComplete) {
+            createTracker: function (isComplete) {
                 return new WorkTracker(isComplete);
             }
         };
